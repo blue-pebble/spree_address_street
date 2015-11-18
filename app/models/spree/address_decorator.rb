bf392cfc1efc49c6ed400145998a9f5487c9b7aa
Rick Blommers <rick@blommersit.nl>
2015-11-18 10:27:38 +0100

    Made number a required field

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -2,6 +2,8 @@ Spree::Address.class_eval do
 
   before_validation :build_address1
 
+  validates :number, presence: true
+
 
   def build_address1
     self.address1 = "#{street} #{number} #{number_postfix}".strip
